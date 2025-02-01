a6a57d29b99c8bef90f3445fd0df5308de8ce017
DDury <87566183+DDury@users.noreply.github.com>
2025-02-01 21:44:15 +0900

    macvuecli

--- 4.vuecli/src/app.vue
+++ 4.vuecli/src/app.vue
@@ -3,7 +3,22 @@
 <script>
 const app = {
   data() {
-    return {};
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          Email: "manuel@localhost.com",
+        },
+        {
+          id: "Julie",
+          name: "Julie Jones",
+          phone: "09877 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
   },
   methods: {},
   computed: {},
