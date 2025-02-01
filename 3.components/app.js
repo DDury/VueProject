7fea2e20e2ed6e4b87c15dc823be15ec73c040c8
DDury <87566183+DDury@users.noreply.github.com>
2025-02-01 21:35:11 +0900

    Update app.js

---
+++ 3.components/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          Email: "manuel@localhost.com",
+        },
+        {
+          id: "Julie",
+          name: "Julie Jones",
+          phone: "09877 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+});
