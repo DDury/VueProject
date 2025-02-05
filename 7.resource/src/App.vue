f0d474dd01604728fd2c5673881acbb8704c9c1e
DDury <87566183+DDury@users.noreply.github.com>
2025-02-05 22:55:05 +0900

    studyResources

---
+++ 7.resource/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storeResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    >
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'official guide',
+          description: 'The official Vue.js doc',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
